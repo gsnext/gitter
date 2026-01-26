065479b994035fcb5127cefb8d59ab423c1d3ba3
Gagan Sharma <gsnexxt@gmail.com>
2026-01-25 19:07:40 -0800

    Day 5: If Statements - 2026-01-25

---
+++ lessons/day_005_conditionals_custom.py
@@ -0,0 +1,26 @@
+"""
+Day 5: If Statements
+
+Learning Objectives:
+- Understand if statements
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about If Statements
+"""
+
+# If Statements Example
+print("Learning about If Statements")
+
+# TODO: Add your code here to practice if statements
+
+# Example code
+def example_function():
+    """Demonstrate if statements"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for if statements
+# TODO: Experiment with different variations
+# TODO: Test your understanding
