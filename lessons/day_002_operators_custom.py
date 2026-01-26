1825b9e57da2af9ddf230d3453fada19f6af26d5
Gagan Sharma <gsnexxt@gmail.com>
2026-01-25 19:07:54 -0800

    Day 2: Operators and Expressions - 2026-01-25

---
+++ lessons/day_002_operators_custom.py
@@ -0,0 +1,26 @@
+"""
+Day 2: Operators and Expressions
+
+Learning Objectives:
+- Understand operators and expressions
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about Operators and Expressions
+"""
+
+# Operators and Expressions Example
+print("Learning about Operators and Expressions")
+
+# TODO: Add your code here to practice operators and expressions
+
+# Example code
+def example_function():
+    """Demonstrate operators and expressions"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for operators and expressions
+# TODO: Experiment with different variations
+# TODO: Test your understanding
