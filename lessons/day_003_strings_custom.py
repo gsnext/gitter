a9797c89322ca8934e6e77e7e7ca83522c842a25
Gagan Sharma <gsnexxt@gmail.com>
2026-01-25 19:07:50 -0800

    Day 3: String Operations - 2026-01-25

---
+++ lessons/day_003_strings_custom.py
@@ -0,0 +1,26 @@
+"""
+Day 3: String Operations
+
+Learning Objectives:
+- Understand string operations
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about String Operations
+"""
+
+# String Operations Example
+print("Learning about String Operations")
+
+# TODO: Add your code here to practice string operations
+
+# Example code
+def example_function():
+    """Demonstrate string operations"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for string operations
+# TODO: Experiment with different variations
+# TODO: Test your understanding
