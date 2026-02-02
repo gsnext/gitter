f91630e2e57635b7381a47d37aa02971ae8ab8c7
Gagan Sharma <gsnexxt@gmail.com>
2026-02-01 22:58:04 -0800

    Day 13: List Methods - 2026-02-01

---
+++ lessons/day_013_list_methods.py
@@ -0,0 +1,26 @@
+"""
+Day 13: List Methods
+
+Learning Objectives:
+- Understand list methods
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about List Methods
+"""
+
+# List Methods Example
+print("Learning about List Methods")
+
+# TODO: Add your code here to practice list methods
+
+# Example code
+def example_function():
+    """Demonstrate list methods"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for list methods
+# TODO: Experiment with different variations
+# TODO: Test your understanding
