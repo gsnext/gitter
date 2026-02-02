2b95e5968daaf1ffbecd1b81ab6078c7e2dd060d
Gagan Sharma <gsnexxt@gmail.com>
2026-02-01 22:58:46 -0800

    Day 14: String Methods - 2026-02-01

---
+++ lessons/day_014_string_methods.py
@@ -0,0 +1,26 @@
+"""
+Day 14: String Methods
+
+Learning Objectives:
+- Understand string methods
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about String Methods
+"""
+
+# String Methods Example
+print("Learning about String Methods")
+
+# TODO: Add your code here to practice string methods
+
+# Example code
+def example_function():
+    """Demonstrate string methods"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for string methods
+# TODO: Experiment with different variations
+# TODO: Test your understanding
