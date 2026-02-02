565a8ab2807aee21f3571e9c549a4f26eb03af1f
Gagan Sharma <gsnexxt@gmail.com>
2026-02-02 00:28:39 -0800

    Day 16: Type Conversion - 2026-02-02

---
+++ lessons/day_016_type_conversion.py
@@ -0,0 +1,26 @@
+"""
+Day 16: Type Conversion
+
+Learning Objectives:
+- Understand type conversion
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about Type Conversion
+"""
+
+# Type Conversion Example
+print("Learning about Type Conversion")
+
+# TODO: Add your code here to practice type conversion
+
+# Example code
+def example_function():
+    """Demonstrate type conversion"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for type conversion
+# TODO: Experiment with different variations
+# TODO: Test your understanding
