b15f643f211467e367eb80c76c7e285ca6ee572c
Gagan Sharma <gsnexxt@gmail.com>
2026-02-02 00:28:25 -0800

    Day 15: User Input and Output - 2026-02-02

---
+++ lessons/day_015_input_output.py
@@ -0,0 +1,26 @@
+"""
+Day 15: User Input and Output
+
+Learning Objectives:
+- Understand user input and output
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about User Input and Output
+"""
+
+# User Input and Output Example
+print("Learning about User Input and Output")
+
+# TODO: Add your code here to practice user input and output
+
+# Example code
+def example_function():
+    """Demonstrate user input and output"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for user input and output
+# TODO: Experiment with different variations
+# TODO: Test your understanding
