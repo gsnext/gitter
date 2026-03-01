d2e989595a6a4f53bca91c0a4dbb857301b697c3
Gagan Sharma <gsnexxt@gmail.com>
2026-02-28 23:28:30 -0800

    Day 17: Boolean Logic - 2026-02-28

---
+++ lessons/day_017_boolean_logic.py
@@ -0,0 +1,26 @@
+"""
+Day 17: Boolean Logic
+
+Learning Objectives:
+- Understand boolean logic
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about Boolean Logic
+"""
+
+# Boolean Logic Example
+print("Learning about Boolean Logic")
+
+# TODO: Add your code here to practice boolean logic
+
+# Example code
+def example_function():
+    """Demonstrate boolean logic"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for boolean logic
+# TODO: Experiment with different variations
+# TODO: Test your understanding
