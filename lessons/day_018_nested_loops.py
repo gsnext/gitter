b80693ecefcba7031f3ef04bf743949984e740c3
Gagan Sharma <gsnexxt@gmail.com>
2026-03-01 00:24:51 -0800

    Day 18: Nested Loops - 2026-03-01

---
+++ lessons/day_018_nested_loops.py
@@ -0,0 +1,26 @@
+"""
+Day 18: Nested Loops
+
+Learning Objectives:
+- Understand nested loops
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about Nested Loops
+"""
+
+# Nested Loops Example
+print("Learning about Nested Loops")
+
+# TODO: Add your code here to practice nested loops
+
+# Example code
+def example_function():
+    """Demonstrate nested loops"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for nested loops
+# TODO: Experiment with different variations
+# TODO: Test your understanding
