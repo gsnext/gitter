0139e25d86eacdb1875fd82703da288913e81a90
Gagan Sharma <gsnexxt@gmail.com>
2026-03-01 00:25:02 -0800

    Day 19: Range Function - 2026-03-01

---
+++ lessons/day_019_range_function.py
@@ -0,0 +1,26 @@
+"""
+Day 19: Range Function
+
+Learning Objectives:
+- Understand range function
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about Range Function
+"""
+
+# Range Function Example
+print("Learning about Range Function")
+
+# TODO: Add your code here to practice range function
+
+# Example code
+def example_function():
+    """Demonstrate range function"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for range function
+# TODO: Experiment with different variations
+# TODO: Test your understanding
