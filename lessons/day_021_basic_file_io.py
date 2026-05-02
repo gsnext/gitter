ecf93fd5345f8c1ee59e70309ce18cff26825cb7
Gagan Sharma <gsnexxt@gmail.com>
2026-05-02 02:11:13 -0700

    Day 21: File Reading - 2026-05-02

---
+++ lessons/day_021_basic_file_io.py
@@ -0,0 +1,26 @@
+"""
+Day 21: File Reading
+
+Learning Objectives:
+- Understand file reading
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about File Reading
+"""
+
+# File Reading Example
+print("Learning about File Reading")
+
+# TODO: Add your code here to practice file reading
+
+# Example code
+def example_function():
+    """Demonstrate file reading"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for file reading
+# TODO: Experiment with different variations
+# TODO: Test your understanding
