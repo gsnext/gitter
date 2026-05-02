41de3e3be0b8526350908de3baf57f11beea2f04
Gagan Sharma <gsnexxt@gmail.com>
2026-05-02 02:03:30 -0700

    Day 20: Enumerate and Zip - 2026-05-02

---
+++ lessons/day_020_enumerate_zip.py
@@ -0,0 +1,26 @@
+"""
+Day 20: Enumerate and Zip
+
+Learning Objectives:
+- Understand enumerate and zip
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about Enumerate and Zip
+"""
+
+# Enumerate and Zip Example
+print("Learning about Enumerate and Zip")
+
+# TODO: Add your code here to practice enumerate and zip
+
+# Example code
+def example_function():
+    """Demonstrate enumerate and zip"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for enumerate and zip
+# TODO: Experiment with different variations
+# TODO: Test your understanding
