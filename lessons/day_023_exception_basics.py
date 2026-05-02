34e1ad386b69a7074091cc4289c264d0e4a59856
Gagan Sharma <gsnexxt@gmail.com>
2026-05-02 02:14:25 -0700

    Day 23: Exception Handling Basics - 2026-05-02

---
+++ lessons/day_023_exception_basics.py
@@ -0,0 +1,26 @@
+"""
+Day 23: Exception Handling Basics
+
+Learning Objectives:
+- Understand exception handling basics
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about Exception Handling Basics
+"""
+
+# Exception Handling Basics Example
+print("Learning about Exception Handling Basics")
+
+# TODO: Add your code here to practice exception handling basics
+
+# Example code
+def example_function():
+    """Demonstrate exception handling basics"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for exception handling basics
+# TODO: Experiment with different variations
+# TODO: Test your understanding
