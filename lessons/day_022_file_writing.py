a77eb04cb2102e0b80f02bc14fea6b771d25a626
Gagan Sharma <gsnexxt@gmail.com>
2026-05-02 02:11:20 -0700

    Day 22: File Writing - 2026-05-02

---
+++ lessons/day_022_file_writing.py
@@ -0,0 +1,26 @@
+"""
+Day 22: File Writing
+
+Learning Objectives:
+- Understand file writing
+- Practice core concepts
+- Apply knowledge to solve problems
+
+Expected Output:
+Learning about File Writing
+"""
+
+# File Writing Example
+print("Learning about File Writing")
+
+# TODO: Add your code here to practice file writing
+
+# Example code
+def example_function():
+    """Demonstrate file writing"""
+    pass
+
+# Practice Exercise
+# TODO: Complete the exercises for file writing
+# TODO: Experiment with different variations
+# TODO: Test your understanding
